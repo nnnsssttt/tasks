9ccacb57aa810db53b8ee05196ca94d2233def21
nnnsssttt <nnnssstttn@mail.ru>
2025-05-05 19:46:33 +0300

    Add files via upload

---
+++ 123/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class Calculator
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Простой калькулятор");
+
+        while (true)
+        {
+            Console.WriteLine("Введите первое число:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите оператор (+, -, *, /):");
+            char op = Console.ReadKey().KeyChar;
+            Console.WriteLine(); // Перенос строки после ввода оператора
+
+            Console.WriteLine("Введите второе число:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("Ошибка: деление на ноль!");
+                        continue; // Переход к следующей итерации цикла
+                    }
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Ошибка: неверный оператор!");
+                    continue; // Переход к следующей итерации цикла
+            }
+
+            Console.WriteLine($"Результат: {result}");
+
+            Console.WriteLine("Продолжить? (да/нет)");
+            string answer = Console.ReadLine();
+            if (answer.ToLower() != "да")
+            {
+                break; // Выход из цикла
+            }
+        }
+    }
+}
---
+++ novela/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+public class TextAdventure
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Затерянный в лесу");
+
+        
+        Console.WriteLine("Ты просыпаешься в лесу. Что будешь делать?");
+        Console.WriteLine("a) Пойдешь на север (компас)");
+        Console.WriteLine("b) Пойдешь на запад (закат)");
+        Console.WriteLine("c) Заберешься на дерево");
+
+        char choice1 = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        switch (choice1)
+        {
+            case 'a':
+                
+                Console.WriteLine("Ты нашел тропинку. Что дальше?");
+                Console.WriteLine("a) Идти по тропинке");
+                Console.WriteLine("b) Свернуть в лес");
+
+                char choice2a = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                if (choice2a == 'a')
+                {
+                    
+                    Console.WriteLine("Ты нашел хижину. Постучишь? (да/нет)");
+                    string choice3aa = Console.ReadLine().ToLower();
+                    if (choice3aa == "да")
+                    {
+                        Console.WriteLine("Тебя встретил дружелюбный отшельник и показал дорогу."); 
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ты ушел от хижины и заблудился."); 
+                    }
+                }
+                else if (choice2a == 'b')
+                {
+                    
+                    Console.WriteLine("Ты нашел светящийся цветок. Прикоснёшься? (да/нет)");
+                    string choice3ab = Console.ReadLine().ToLower();
+                    if (choice3ab == "да")
+                    {
+                        Console.WriteLine("Цветок оказался магическим и перенес тебя домой!"); 
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ты прошел мимо цветка и заблудился."); 
+                    }
+                }
+                break;
+
+
+            case 'b':
+               
+                Console.WriteLine("Ты в болоте. Что делать?");
+                Console.WriteLine("a) Выбраться самому");
+                Console.WriteLine("b) Позвать на помощь");
+                
+                break;
+
+            case 'c':
+               
+                Console.WriteLine("Ты на дереве. Видишь дым. Что делать?");
+                Console.WriteLine("a) Идти к дыму");
+                Console.WriteLine("b) Остаться на дереве");
+               
+                break;
+
+            default:
+                Console.WriteLine("Неверный выбор.");
+                break;
+        }
+
+
+        Console.ReadKey(); 
+    }
+}
